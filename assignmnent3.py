97d79ca919919616ee10eb237d62341a049d5c23
ReedMyers715 <reed.myers@bsu.edu>
2025-09-04 15:45:28 +0000

    added input function

--- assignmnent3.py
+++ assignmnent3.py
@@ -1,3 +1,11 @@
 def Main():
     file = open("students.txt", "r")
     content = file.read()
+    print("Choose an option")
+    print("1) Search by Last Name")
+    print("2) Search by Major")
+    print("3) Quit")
+
+    answer = int(input(""))
+
+
