31e1146b9063ef5af5120e6be1eaf585baf27b2a
ReedMyers715 <reed.myers@bsu.edu>
2025-09-09 17:05:48 +0000

    File is saved as dictionary in the wierdest way possible

--- assignmnent3.py
+++ assignmnent3.py
@@ -1,13 +1,24 @@
 def Main():
     with open("students.txt", "r") as file:
-        content = file.read()
-        while True:
-            print("1) Search by Last Name")
-            print("2) Search by Major")
-            print("3) Quit")
-            answer = int(input("Choose an option: "))
-            if answer == 1:
-                lastname = input("Enter last name: ")
+        for line in file:
+            if line != "\n":
+                if line.endswith("\n"):
+                    line = line[:-1]
+                else:
+                    parts = line.split(",")
+                    studentId = parts[0]
+                    lastName = parts[1]
+                    firstName = parts[2]
+                    major = parts[3]
+                    gpa = parts[4]
+    while True:
+        print("Choose an option: ")
+        print("1) Search by Last Name")
+        print("2) Search by Major")
+        print("3) Quit")
+        answer = int(input("Enter your choice: "))
+        if answer == 1:
+            lastname = input("Enter last name: ").split()
                 
                 
 Main()
